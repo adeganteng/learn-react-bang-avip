f9706ba97560537e8d193259941ceaa1a22a701a
adeganteng <wicaksonoade21@gmail.com>
2024-08-23 06:45:54 +0700

    1. Setup React with Vite and Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-teal-400 flex justify-center items-center">
+        <h1 className="text-3xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
