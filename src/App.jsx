9bab4eee6173c23729cc50da2a9b1f997f5a91c8
adeganteng <wicaksonoade21@gmail.com>
2024-08-23 07:38:00 +0700

    2. Component & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+//! contoh mendefinisikan Component menggunakan Class Component
+// class Button extends React.Component {
+//   render() {
+//     return (
+//       <button className="h-10 px-6 font-semibold rounded-md bg-black text-white">
+//         Buy Now
+//       </button>
+//     );
+//   }
+// }
+
+//! contoh mendefinisikan Component menggunakan Function Component
+const Button = ({
+  variant = "bg-emerald-700 text-white",
+  text = "Button default",
+}) => {
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant}`}>
+      {text}
+    </button>
+  );
+};
+
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-teal-400 flex justify-center items-center">
+      <div className="min-h-screen bg-teal-400 flex flex-col gap-3 justify-center items-center">
         <h1 className="text-3xl font-bold">Hello World</h1>
+        <div className="flex justify-center items-center gap-3">
+          <Button variant={"bg-black text-white"} text={"Button Black"} />
+          <Button variant={"bg-red-700 text-white"} text={"Button Red"} />
+          <Button variant={"bg-blue-700 text-white"} text={"Button Blue"} />
+          <Button />
+        </div>
       </div>
     </>
   );
